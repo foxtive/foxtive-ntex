e35ec22dd226cd40eb50b8b54ad140330676f249
GitHub Action <action@github.com>
2025-06-14 16:49:27 +0000

    style(cargo-fmt): apply changes

--- foxtive-ntex/src/http/extractors/mod.rs
+++ foxtive-ntex/src/http/extractors/mod.rs
@@ -1,16 +1,15 @@
 mod byte_body;
 mod client_info;
+mod de_json_body;
 mod json_body;
 #[cfg(feature = "jwt")]
 mod jwt_auth_token;
 mod string_body;
-mod de_json_body;
-
 
 pub use byte_body::ByteBody;
 pub use client_info::ClientInfo;
-pub use json_body::JsonBody;
-pub use string_body::StringBody;
 pub use de_json_body::DeJsonBody;
+pub use json_body::JsonBody;
 #[cfg(feature = "jwt")]
 pub use jwt_auth_token::JwtAuthToken;
+pub use string_body::StringBody;
