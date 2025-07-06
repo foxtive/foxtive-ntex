ccb1edbd314f9afc1e2361a8c115b196c0cff9af
ahmard <ahmard.mu@gmail.com>
2025-07-06 05:18:02 +0100

    feat(data-input): add 'inner', 'into_inner', 'parts', 'into_parts' methods

--- foxtive-ntex-multipart/CHANGELOG.md
+++ foxtive-ntex-multipart/CHANGELOG.md
@@ -3,6 +3,9 @@ foxtive-ntex-multipart changelog file
 
 ------
 
+### 0.3.0 (2025-06-06)
+* feat(data-input): add 'inner', 'into_inner', 'parts', 'into_parts' methods
+
 ### 0.3.0 (2025-06-05)
 * bump(crates): to latest versions
 
--- foxtive-ntex-multipart/src/data_input.rs
+++ foxtive-ntex-multipart/src/data_input.rs
@@ -10,4 +10,20 @@ impl DataInput {
     pub fn get<T: FromStr>(&self) -> Result<T, T::Err> {
         self.value.parse::<T>()
     }
+
+    pub fn inner(&self) -> &DataInput {
+        self
+    }
+
+    pub fn into_inner(self) -> DataInput {
+        self
+    }
+
+    pub fn parts(&self) -> (&String, &String) {
+        (&self.name, &self.value)
+    }
+
+    pub fn into_parts(self) -> (String, String) {
+        (self.name, self.value)
+    }
 }
