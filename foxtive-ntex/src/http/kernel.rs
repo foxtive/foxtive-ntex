d1f884705a25b508c6f4536a6d1f4cdb3cde0c79
GitHub Action <action@github.com>
2026-02-26 13:00:10 +0000

    style(cargo-fmt): apply changes

--- foxtive-ntex/src/http/kernel.rs
+++ foxtive-ntex/src/http/kernel.rs
@@ -36,7 +36,7 @@ pub fn register_routes(config: &mut ServiceConfig, routes: Vec<Route>) {
                 config.service(
                     scope
                         .middleware(MiddlewareChain::new(route.middlewares.clone()))
-                        .configure(controller.handler)
+                        .configure(controller.handler),
                 );
             } else {
                 config.service(scope.configure(controller.handler));
